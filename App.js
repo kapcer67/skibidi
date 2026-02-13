1fdc89d035fb489cd2c04f47ec05c89a4da54225
kapcer67 <kbialy205@gmail.com>
2026-02-13 14:46:51 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,37 @@
+import image1 from './lebandit.jpg'
+import image2 from './zeus.jpg'
+import image3 from './plinko.jpg'
+import image4 from './laced.jpg'
+import './App.css';
+
 function App() {
+  function info1(){
+    alert("Le Bandit to klaszycny i najbardziej znany slot na kasynach online. Rzadko oddaje.")
+  }
+  function info2(){
+    alert("Le Bandit to klaszycny i najbardziej znany slot na kasynach online. Rzadko oddaje.")
+  }
+  function info3(){
+    alert("Le Bandit to klaszycny i najbardziej znany slot na kasynach online. Rzadko oddaje.")
+  }
+  function info4(){
+    alert("Le Bandit to klaszycny i najbardziej znany slot na kasynach online. Rzadko oddaje.")
+  }
   return (
     <>
-       <h1>Skibidi projekt</h1>
+    <center>
+      <h1>Najlepsze sloty na kasynach online</h1><br></br><br></br>
+      <table>
+        <tr>
+          <td><img src={image1} onClick={info1}></img></td>
+          <td><img src={image2}></img></td>
+        </tr>
+        <tr>
+          <td><img src={image3}></img></td>
+          <td><img src={image4}></img></td>
+        </tr>
+      </table>
+      </center>
     </>
   );
 }
